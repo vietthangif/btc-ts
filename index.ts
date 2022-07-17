697081f8de97dcf6c45041a1f86993d537bcfeda
vietthangif <vietthangif@gmail.com>
2022-07-18 00:03:25 +0700

    Init

---
+++ index.ts
@@ -0,0 +1,11 @@
+export const genPrivateKey = (): string => {
+  return '';
+};
+
+export const genPublicKey = (privateKey: string): string => {
+  return '';
+};
+
+export const genAddress = (publicKey: string): string => {
+  return '';
+};
